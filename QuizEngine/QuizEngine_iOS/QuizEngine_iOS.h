99346a375b3e486fe07e0e2a8a76332670333816
andremartingo <andre.martingo@gmail.com>
2018-07-13 10:48:25 +0100

    Add QuizEngine to QuizApp

---
+++ QuizEngine/QuizEngine_iOS/QuizEngine_iOS.h
@@ -0,0 +1,19 @@
+//
+//  QuizEngine_iOS.h
+//  QuizEngine_iOS
+//
+//  Created by André Martingo on 13/07/2018.
+//  Copyright © 2018 André Martingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuizEngine_iOS.
+FOUNDATION_EXPORT double QuizEngine_iOSVersionNumber;
+
+//! Project version string for QuizEngine_iOS.
+FOUNDATION_EXPORT const unsigned char QuizEngine_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizEngine_iOS/PublicHeader.h>
+
+
